577c1ef31564b941c014a76c8b036d1a8fd2ad1d
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-01 13:49:50 +0800

    Add files via upload

---
+++ Exercise 2/Main.java
@@ -0,0 +1,33 @@
+package Main;
+
+public class Main {
+public static void main(String[] args) {
+		
+		Bird Eagle= new Bird(); 
+		
+		System.out.println("Bird type: "+Eagle.Type);
+		System.out.println("Bird Size: "+Eagle.Size);
+		System.out.println("Feathers: "+Eagle.Feathers);
+		System.out.println("Number of legs: "+Eagle.NumOfLegs);
+		System.out.println();
+		
+		Eagle.PrintEat();
+		Eagle.PrintSleeps();
+		Eagle.PrintLayEggs();
+		System.out.println();
+		
+		Fish Salmon= new Fish(); 
+		
+		System.out.println("Fish type: "+Salmon.Type);
+		System.out.println("Fish Size: "+Salmon.Size);
+		System.out.println("Blood: "+Salmon.Blood);
+		System.out.println("Number of fins: "+Salmon.NumOfFins);
+		System.out.println();
+		
+		Salmon.PrintEat();
+		Salmon.PrintSwim();
+		Salmon.PrintBreath();
+		System.out.println();
+		
+	}
+}
