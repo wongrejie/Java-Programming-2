3fb4a1d576f89c62ec1df466b60d2aba02d55f8c
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-05 18:10:12 +0800

    Update Main.java

--- Exercise 2.2/Main.java
+++ Exercise 2.2/Main.java
@@ -13,7 +13,7 @@ public static void main(String[] args) {
 		System.out.println("====================================");
 		System.out.println("Eagle characteristics");
 		System.out.println("====================================");
-		Eagle.printChar("Bald eagle","Medium","Yes",2,6.3,0.9);
+		Eagle.printChar("Bald eagle","Medium","Yes",20,2,6.3,0.9);
 		Eagle.printCalWeight(6.3, 5);
 		Eagle.printCalHeight(0.9, 5);
 		Eagle.BirdBehaviour("Eagle");
@@ -21,7 +21,7 @@ public static void main(String[] args) {
 		System.out.println("====================================");
 		System.out.println("Ostrich characteristics");
 		System.out.println("====================================");
-		Ostrich.printChar("Southern Ostrich","Big","Yes",2,140,2.8);
+		Ostrich.printChar("Southern Ostrich","Big","Yes",45,2,140,2.8);
 		Ostrich.printCalWeight(140, 2);
 		Ostrich.printCalHeight(2.8, 2);
 		Ostrich.BirdBehaviour("Ostrich");
@@ -29,7 +29,7 @@ public static void main(String[] args) {
 		System.out.println("====================================");
 		System.out.println("HummingBird characteristics");
 		System.out.println("====================================");
-		HummingBird.printChar("Rufous HummingBird","Small","Yes",2,0.003,0.7);
+		HummingBird.printChar("Rufous HummingBird","Small","Yes",5,2,0.003,0.7);
 		HummingBird.printCalWeight(0.003, 25);
 		HummingBird.printCalHeight(0.7, 25);
 		HummingBird.BirdBehaviour("HummingBird");
@@ -38,11 +38,10 @@ public static void main(String[] args) {
 		System.out.println("====================================");
 		System.out.println("Hornbill characteristics");
 		System.out.println("====================================");
-		Hornbill.printChar("Rhinoceros Hornbill","Medium","Yes",2,2.96,1.3);
+		Hornbill.printChar("Rhinoceros Hornbill","Medium","Yes",35,2,2.96,1.3);
 		Hornbill.printCalWeight(2.96, 14);
 		Hornbill.printCalHeight(1.3, 14);
 		Hornbill.BirdBehaviour("Hornbill");
-		
 			
 	}
 }
