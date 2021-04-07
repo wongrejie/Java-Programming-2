255b8214384f2ea522c5d09259ccda9005940b54
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-07 09:43:15 +0800

    Update Main.java

--- Exercise 2.2/Main.java
+++ Exercise 2.2/Main.java
@@ -15,7 +15,7 @@ public static void main(String[] args) {
 		Eagle.printChar("Bald eagle","Medium","Yes",20,2,6.3,0.9);
 		Eagle.printCalWeight(6.3, 5);
 		Eagle.printCalHeight(0.9, 5);
-		Eagle.printCalFoodExpenses(56, 8);
+		Eagle.printCalFoodExpenses(56, 8,3);
 		
 		System.out.println("====================================");
 		System.out.println("Ostrich characteristics");
@@ -23,7 +23,7 @@ public static void main(String[] args) {
 		Ostrich.printChar("Southern Ostrich","Big","Yes",45,2,140,2.8);
 		Ostrich.printCalWeight(140, 2);
 		Ostrich.printCalHeight(2.8, 2);
-		Ostrich.printCalFoodExpenses(100,6);
+		Ostrich.printCalFoodExpenses(100,6,2);
 		
 		System.out.println("====================================");
 		System.out.println("HummingBird characteristics");
@@ -31,7 +31,7 @@ public static void main(String[] args) {
 		HummingBird.printChar("Rufous HummingBird","Small","Yes",5,2,0.003,0.7);
 		HummingBird.printCalWeight(0.003, 25);
 		HummingBird.printCalHeight(0.7, 25);
-		HummingBird.printCalFoodExpenses(1,9);
+		HummingBird.printCalFoodExpenses(1,9,4);
 		
 		
 		System.out.println("====================================");
@@ -40,7 +40,7 @@ public static void main(String[] args) {
 		Parrot.printChar("Cockatoo","Medium","Yes",15,2,0.12,0.6);
 		Parrot.printCalWeight(0.12, 14);
 		Parrot.printCalHeight(0.6, 14);
-		Parrot.printCalFoodExpenses(40,7);
+		Parrot.printCalFoodExpenses(40,7,5);
 			
 	}
 }
