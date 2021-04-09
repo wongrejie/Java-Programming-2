1a01ce4cb29bf43da551ccb8d768e8cb2a8b2065
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-09 17:57:57 +0800

    Update Main.java

--- Exercise 2.3/Main.java
+++ Exercise 2.3/Main.java
@@ -7,9 +7,7 @@ public class Main {
 public static void main(String[] args) {
 		
 		Bird Eagle= new Bird(); 
-//		Bird Ostrich= new Bird();
-//		Bird HummingBird= new Bird();
-//		Bird Parrot= new Bird();
+
 		
 		Eagle.printChar();
 		Eagle.printWeight();
