f81a4b5083a3eee14dcd65a8d504fd9d4736149e
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-09 18:04:17 +0800

    Update Main.java

--- Exercise 2.3/Main.java
+++ Exercise 2.3/Main.java
@@ -11,24 +11,28 @@ public static void main(String[] args) {
 		Bird HummingBird= new Bird();
 		Bird Parrot= new Bird();
 		
+		System.out.println("Information for Eagle");
 		Eagle.printChar();
 		Eagle.printWeight();
 		Eagle.BirdBehaviour();
 		Eagle.printYearOfBorn();
 		Eagle.printCalFoodExpenses();
 		
+		System.out.println("Information for Ostrich");
 		Ostrich.printChar();
 		Ostrich.printWeight();
 		Ostrich.BirdBehaviour();
 		Ostrich.printYearOfBorn();
 		Ostrich.printCalFoodExpenses();	
 		
+		System.out.println("Information for Hummingbird");
 		HummingBird.printChar();
 		HummingBird.printWeight();
 		HummingBird.BirdBehaviour();
 		HummingBird.printYearOfBorn();
 		HummingBird.printCalFoodExpenses();	
 		
+		System.out.println("Information for Parrot");
 		Parrot.printChar();
 		Parrot.printWeight();
 		Parrot.BirdBehaviour();
