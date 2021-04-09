2c9d2fc2def16af0e378973ddd50b8533838936d
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-09 17:57:22 +0800

    Add files via upload

---
+++ Exercise 2.3/Bird.java
@@ -0,0 +1,107 @@
+package Main;
+
+import java.util.Scanner;
+
+
+public class Bird {	//declare variables in class.
+	
+	 String type,size,fly,swim;
+	 int lifespan,quantity,age,YearOfBorn,MonthlyExp,MonthQuantity,BirdQuantity,EstimateTimeLeft;
+	 double weight,TotalWeight, totalFoodExpenses,FoodExpensesDaily,height;
+	
+	static Scanner s = new Scanner(System.in); //create a scanner object.
+	
+	void printChar() {
+		System.out.println("Bird type: ");
+		 type =s.nextLine();
+		
+		System.out.println("Size (big/medium/small): ");
+		 size =s.nextLine();
+		
+		System.out.println("Enter Height of the bird(meter): ");
+		height =s.nextDouble();
+		
+		System.out.println("\nBird type: "+ type);
+		System.out.println("Size: "+size);
+		System.out.println("the height of this bird is about "+height+" meter\n");
+		
+		
+			
+	}
+	
+	void BirdBehaviour() {
+		
+		System.out.println("\nCan this bird fly?(yes/no)");
+		 s.nextLine();
+		 fly =s.nextLine();
+		 System.out.println("Can this bird swim?(yes/no)");
+		 swim =s.nextLine();
+		
+		switch(fly) {
+		case "yes" : System.out.println("\nthis bird can travel by flying.\n");
+						break;
+		case "no" : System.out.println("this bird cannot fly and it can only move on ground.\\n");
+						break;
+		default : 	System.out.println("Please choose only yes and no.\n");
+						break;
+					}
+		
+		switch(swim) {
+		case "yes" : System.out.println("this bird can hunt their prey underwater.\n");
+						break;
+		case "no" : System.out.println("this bird cannot swim.\n");
+						break;
+		default : 	System.out.println("Please choose only yes and no./n");
+						break;
+					}
+		}
+	
+	void printWeight() {
+		
+		System.out.println("Enter the weight of this bird:");
+		 weight =s.nextDouble();
+		System.out.println("Enter the quantity of this bird:");
+		quantity =s.nextInt();
+		
+		TotalWeight= weight*quantity;
+		System.out.printf("Total weight for "+quantity+" bird of this type can weight up to %.2f",TotalWeight);
+		System.out.print(" kg\n");
+	}
+	 
+	void printYearOfBorn() {
+		
+		System.out.println("\nEnter the age of the bird:");
+		age =s.nextInt();
+		System.out.println("Enter the Lifespan (Years) for this type of bird:");
+		lifespan =s.nextInt();
+		
+		YearOfBorn = 2021-age;
+		EstimateTimeLeft=lifespan-age;
+		
+		System.out.println("\nThe year of born for this bird is "+YearOfBorn);
+		System.out.println("The estimated time left for this bird to live is about "+EstimateTimeLeft+" years");
+	}
+	
+	void printCalFoodExpenses() {
+		
+		System.out.println("\nEnter the monthly food expenses for ONE bird(RM):");
+		 MonthlyExp =s.nextInt();
+		
+		System.out.println("Enter how many months for the expenses:");
+		MonthQuantity =s.nextInt();
+		
+		System.out.println("Enter the quantity of birds:");
+		BirdQuantity =s.nextInt();
+
+		
+		totalFoodExpenses= (MonthlyExp*MonthQuantity)*BirdQuantity;
+		
+		FoodExpensesDaily= totalFoodExpenses/30;
+		 		
+			System.out.println("\nThe food expenses for "+BirdQuantity+" bird of this type can cost up to "+" RM"+
+								totalFoodExpenses+" for "+MonthQuantity+" months.");
+			System.out.printf("And up to RM %.2f",FoodExpensesDaily);
+			System.out.print(" for each day.");
+	}
+	
+}
---
+++ Exercise 2.3/Main.java
@@ -0,0 +1,21 @@
+package Main;
+
+
+public class Main {
+
+
+public static void main(String[] args) {
+		
+		Bird Eagle= new Bird(); 
+//		Bird Ostrich= new Bird();
+//		Bird HummingBird= new Bird();
+//		Bird Parrot= new Bird();
+		
+		Eagle.printChar();
+		Eagle.printWeight();
+		Eagle.BirdBehaviour();
+		Eagle.printYearOfBorn();
+		Eagle.printCalFoodExpenses();	
+			
+	}
+}
