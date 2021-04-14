c44a00cbeee7eec7083aa174355face33028d9ca
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-14 22:12:09 +0800

    Add files via upload

---
+++ exercise2.4/Main.java
@@ -0,0 +1,34 @@
+package exercise24;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fish Salmon= new Fish();
+		
+
+		
+		Salmon.setFishType("Atlantic Salmon.");
+		Salmon.setColour("Silver.");
+		Salmon.setFinNum(8);
+		Salmon.setBlood("cold-blooded");
+		Salmon.setWeight(26.5);
+		Salmon.setLifespan(7);
+		Salmon.setFoodExpenses(1000);
+		
+		
+		System.out.println("Type : " + Salmon.getFishType());
+		System.out.println("Colour: "+ Salmon.getColour());
+		System.out.println("Number of fins: "+ Salmon.getFinNum());
+		System.out.println("Bloodtype: "+ Salmon.getBlood());
+		System.out.println("Weight(kg): "+Salmon.getWeight());
+		System.out.println("Lifespan for this animal is about " + Salmon.getLifespan()+" years");
+		System.out.println("Monthly food expenses: RM"+Salmon.getFoodExpenses());
+		
+		System.out.println("Estimated time left for this aquatic animal to live is about "+Salmon.countTimeLeft(5)+" years");
+		
+		
+	}
+
+}
