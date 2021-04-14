3c3255738b7b6bee8063ab4dbf7d51cc736ca467
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-14 23:40:51 +0800

    Update Main.java

--- exercise2.4/Main.java
+++ exercise2.4/Main.java
@@ -5,16 +5,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Fish Salmon= new Fish();
-		
-
-		
-		Salmon.setFishType("Atlantic Salmon.");
-		Salmon.setColour("Silver.");
+		Salmon.setFishType("Atlantic Salmon");
+		Salmon.setColour("Silver");
 		Salmon.setFinNum(8);
 		Salmon.setBlood("cold-blooded");
 		Salmon.setWeight(26.5);
 		Salmon.setLifespan(7);
+		Salmon.setAge(5);
 		Salmon.setFoodExpenses(1000);
 		
 		
@@ -24,7 +21,10 @@ public class Main {
 		System.out.println("Bloodtype: "+ Salmon.getBlood());
 		System.out.println("Weight(kg): "+Salmon.getWeight());
 		System.out.println("Lifespan for this animal is about " + Salmon.getLifespan()+" years");
+		System.out.println("The age of this fish is " + Salmon.getAge()+" year's old");
 		System.out.println("Monthly food expenses: RM"+Salmon.getFoodExpenses());
+		
+		System.out.println("Estimated time left for this aquatic animal to live is about "+Salmon.countTimeLeft()+" years");
 		
 		
 		
