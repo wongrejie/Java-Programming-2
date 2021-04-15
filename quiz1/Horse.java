88e3b4aeae0aa0257a5a4466eecf79f57741c64e
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-15 13:35:14 +0800

    Add files via upload

---
+++ quiz1/Horse.java
@@ -0,0 +1,93 @@
+package quiz1;
+import java.util.Scanner;
+public class Horse {
+
+	String horseType,colour,blood;
+	int lifespan,age,estimateTimeLeft;
+	double weight,monthlyExp;
+	
+		Scanner s = new Scanner(System.in);
+	// Create Setter Method
+	  public void setHorseType() {
+		  
+		  System.out.printf("Enter the horse type: ");
+		  this.horseType=s.nextLine();
+		  
+	  }
+	  
+	  public void setColour() {
+		  System.out.printf("Enter the colour of the horse: ");
+			this.colour = s.nextLine();
+	  }
+	  
+	  
+	  
+	  public void setWeight() {
+		  System.out.printf("Enter the weight of this horse(kg): ");
+		  this.weight = s.nextDouble();
+		
+	  }
+	   
+	  public void setLifespan() {
+		  
+		  System.out.printf("Enter the lifespan of this horse: ");
+		  this.lifespan = s.nextInt();
+		  
+			}
+	  
+	  public void setAge() {
+		  
+		  System.out.printf("Enter the age of this horse: ");
+		  this.age = s.nextInt();
+		  
+		}
+ 
+	  public void setFoodExpenses() {
+			
+		  System.out.printf("Enter the monthly food expenses of this horse: RM");
+		  this.monthlyExp = s.nextDouble();
+	  }	
+				
+ 
+	  
+
+	  // Create Getter Method
+	 
+	  
+	  public String getHorseType() {
+		  return this.horseType;
+	  }
+	  
+	  public String getColour() {
+	 	  return this.colour;
+ 	  }
+	  
+	  public double getWeight() {
+		  return this.weight;
+	  }
+	  
+	  public String getBlood() {
+		  return this.blood;
+	  }
+	  
+	  public Integer getLifespan() {
+		  return this.lifespan;
+	  }
+	  
+	  public int getAge() {
+		  return this.age;
+	  }
+	  
+	  public double getFoodExpenses() {
+		  return this.monthlyExp;
+	  }
+	  
+	  
+	  
+	// Create a getTimeLeft() method
+	public Integer getTimeLeft() {
+		
+		return estimateTimeLeft= this.lifespan-age;
+		
+	}
+}
---
+++ quiz1/Main.java
@@ -0,0 +1,43 @@
+package quiz1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Horse horse1 = new Horse();
+		horse1.setHorseType();
+		horse1.setColour();
+		horse1. setWeight();
+		horse1. setLifespan();
+		horse1.setAge();
+		horse1. setFoodExpenses();
+		
+		
+		 System.out.println("\n~~~~~~~~ Horse 1 ~~~~~~~~ ");
+		 System.out.println("Type: "+horse1.getHorseType());
+		 System.out.println("Colour: "+horse1.getColour());
+		 System.out.println("Weight: "+horse1.getWeight()+" kg");
+		 System.out.println("Lifespan: "+horse1.getLifespan()+" years");
+		 System.out.println("Age: " + horse1.getAge()+" year's old");
+		 System.out.println("Estimated time to live is about: "+horse1.getTimeLeft()+" years");
+		 System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~ ");
+	
+	    Horse Horse2 = new Horse();
+		Horse2.setHorseType();
+		Horse2.setColour();
+		Horse2. setWeight();
+		Horse2. setLifespan();
+		Horse2.setAge();
+		Horse2. setFoodExpenses();
+			
+			
+			 System.out.println("\n~~~~~~~~ Horse 2 ~~~~~~~~ ");
+			 System.out.println("Type: "+Horse2.getHorseType());
+			 System.out.println("Colour: "+Horse2.getColour());
+			 System.out.println("Weight: "+Horse2.getWeight()+" kg");
+			 System.out.println("Lifespan: "+Horse2.getLifespan()+" years");
+			 System.out.println("Age: " + Horse2.getAge()+" year's old");
+			 System.out.println("Estimated time to live is about: "+Horse2.getTimeLeft()+" years");
+			 System.out.println("\n~~~~~~~~~~~~~~~~~~~~~~~~ ");
+	}
+
+}
