7d24c6b9b7f4bdf9cfe73e7396f908acf97679d6
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-21 17:45:46 +0800

    Update Main.java

--- Exercise 4/Main.java
+++ Exercise 4/Main.java
@@ -6,9 +6,9 @@ public class Main {
 
 	public static void main(String[]a) {
 		Laptop objLaptop= new Laptop();
-		System.out.println(objLaptop);
+		System.out.println(objLaptop+"\n");
 		
-		Asus objAsus= new Asus("Asus","Turbo","Black","Window 10",2.6, 2500, 15, 5);
+		Asus objAsus= new Asus("Asus","TUF FX505","Black","Window 10",2.2, 4499, 15, 5);
 		System.out.println(objAsus);
 		System.out.printf("\nTotal Price: RM %.2f",objAsus.getTotalPrice());
 		System.out.printf("\nDown Payment: RM %.2f",objAsus.getDownPayment());
