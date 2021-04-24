29161a91f004470f3d3360b1f3d9317d9df1ae8b
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-24 20:53:13 +0800

    Update Main.java

--- Exercise 4.1/Main.java
+++ Exercise 4.1/Main.java
@@ -10,14 +10,14 @@ public class Main {
 		Apple objApple= new Apple("Apple",3,5);
 		System.out.println(objApple);
 		
-		Watermelon objMelon= new Watermelon("Watermelon","Big",8,9);
-		System.out.println(objMelon);
-		
 		GreenApple objGreenApple= new GreenApple("Green Apple","Sour",7,6,10);
 		System.out.println(objGreenApple);
 		
 		RedApple objRedApple= new RedApple("Red Apple","Sweet",6,8,8);
 		System.out.println(objRedApple);
+		
+		Watermelon objMelon= new Watermelon("Watermelon","Big",8,9);
+		System.out.println(objMelon);
 		
 		Strawberry objStrBerry= new Strawberry("Strawberry","Honeoye","Bright Red","Medium",35,0.2);
 		System.out.println(objStrBerry);
