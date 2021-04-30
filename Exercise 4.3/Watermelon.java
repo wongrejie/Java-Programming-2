1dcfe95cd2e2d3266d36d5207a8e2ec097ff505f
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-04-30 18:49:17 +0800

    Update Watermelon.java

--- Exercise 4.3/Watermelon.java
+++ Exercise 4.3/Watermelon.java
@@ -12,11 +12,11 @@ public class Watermelon extends Fruits{	//sub class of Fruits
 		weight=w;
 		price=p;
 		  
-		toString();//call overriding method
-		totalPrice();//call overloading method with no parameter
-		totalPrice(price);//call overloading method with 1 parameter
-		totalPrice(weight,price); //call overloading method with 2 parameter	
-		Benefits();//call overriding method
+		toString();			//call overriding method
+		totalPrice();			//call overloading method with no parameter
+		totalPrice(price);		//call overloading method with 1 parameter
+		totalPrice(weight,price); 	//call overloading method with 2 parameter	
+		Benefits();			//call overriding method
 		
 	}	
 	
