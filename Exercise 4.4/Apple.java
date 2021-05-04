1bbe1896e92a9665aa0d708bc42c19d9438a7a07
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-05-04 14:13:15 +0800

    Update Apple.java

--- Exercise 4.4/Apple.java
+++ Exercise 4.4/Apple.java
@@ -22,6 +22,7 @@ public class Apple extends Fruits { //sub class
 		
 		} 
 		else {
+			discount=0.15; //discount 15%
 			totalPrice(discount,price); //call overloading method with 2 parameter
 			System.out.println("Weight MORE than 5 KG, Total price: RM" + totalPrice(discount,price));
 		}
