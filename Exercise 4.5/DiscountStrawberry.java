b788c90912a430ce7263046642582e86b59d3380
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-05-10 09:18:21 +0800

    Update DiscountStrawberry.java

--- Exercise 4.5/DiscountStrawberry.java
+++ Exercise 4.5/DiscountStrawberry.java
@@ -1,7 +1,8 @@
 package fruitAbstract;
 
+//Discount for Strawberry
  class DiscountStrawberry implements Discount {
-
+	//method for implementation
 		public double discountRate() {
 			return 0.10;
 		}
