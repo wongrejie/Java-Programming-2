fa33a1d43358176ca9913ceda11813b77851de75
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-05-10 09:18:38 +0800

    Update DiscountWatermelon.java

--- Exercise 4.5/DiscountWatermelon.java
+++ Exercise 4.5/DiscountWatermelon.java
@@ -2,7 +2,7 @@ package fruitAbstract;
 
 //Discount for Watermelon
  class DiscountWatermelon implements Discount {
-
+		//method for implementation
 		public double discountRate() {
 			return 0.25;
 		}
