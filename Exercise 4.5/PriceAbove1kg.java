6587869962c93c489de7a6fbbdc3906667ba0d29
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-05-10 09:34:58 +0800

    Add files via upload

---
+++ Exercise 4.5/PriceAbove1kg.java
@@ -0,0 +1,19 @@
+package fruitAbstract;
+
+ class PriceAbove1kg implements NewPrice {
+	
+	public double newPrice() {
+		return 15.00;
+	}
+ }
+ class PriceAbove5kg implements NewPrice {
+		
+		public double newPrice() {
+			return 12.00;
+		}
+}
+
+ 
+
+ 
+
