20438956ab3206d0e0cf25a1dca5ab687bf3788d
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-05-10 09:17:26 +0800

    Update DiscountApple.java

--- Exercise 4.5/DiscountApple.java
+++ Exercise 4.5/DiscountApple.java
@@ -1,22 +1,23 @@
 package fruitAbstract;
 
+//Discount for Apple
  class DiscountApple implements Discount {
-
+		//method implementation
 		public double discountRate() {
 			return 0.15;
 		}
 		
  
 }
-
+//Discount for Green Apple
 class DiscountGreenApple implements Discount {
-
+		//method implementation
 		public double discountRate() {
 		return 0.20;
 	}
-	
+//Discount for Red Apple
 class DiscountRedApple implements Discount {
-
+		//method implementation
 		public double discountRate() {
 			return 0.30;
 		}
