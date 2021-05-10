6f989e62eea462cd0e39db7b4f843107aa78401b
Wong Re Jie <72849000+wongrejie@users.noreply.github.com>
2021-05-10 09:50:40 +0800

    Update PriceAbove1kg.java

--- Exercise 4.5/PriceAbove1kg.java
+++ Exercise 4.5/PriceAbove1kg.java
@@ -1,13 +1,15 @@
 package fruitAbstract;
 
+//new price for Apple above 1kg below 5kg
  class PriceAbove1kg implements NewPrice {
-	
+	//method implementation
 	public double newPrice() {
 		return 15.00;
 	}
  }
+//new price for Apple above 5kg 
  class PriceAbove5kg implements NewPrice {
-		
+		//method implementation
 		public double newPrice() {
 			return 12.00;
 		}
